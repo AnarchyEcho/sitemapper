12053d345ccf08447c0585cbb7824f1a02cc1cf5
kodeandre <andre.kodehode@gmail.com>
2022-07-05 18:03:34 +0200

    removed custom text file, redundant. also docker

--- index.ts
+++ index.ts
@@ -1,18 +1,9 @@
 const SitemapGenerator = require('sitemap-generator');
-import fs from 'fs';
-
-const logger = fs.createWriteStream('output.txt', {
-  flags: 'a',
-});
 
 const site = 'https://gimpinfo.vercel.app';
 
 const generator = SitemapGenerator(site, {
   stripQuerystring: false,
-});
-
-generator.on('add', (url: any) => {
-  logger.write(`${url}\n`);
 });
 
 generator.on('done', () => {
