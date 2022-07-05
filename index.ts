a1a54747bde77b1c09e062a5651eb53bf4dd578d
kodeandre <andre.kodehode@gmail.com>
2022-07-05 20:43:16 +0200

    Changed options

--- index.ts
+++ index.ts
@@ -4,6 +4,9 @@ const site = 'https://gimpinfo.vercel.app';
 
 const generator = SitemapGenerator(site, {
   stripQuerystring: false,
+  maxDepth: 0,
+  filepath: './sitemap.xml',
+  lastMod: true,
 });
 
 generator.on('done', () => {
